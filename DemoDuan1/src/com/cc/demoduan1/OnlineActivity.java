ce7017e84d0b0c9ee50fa60c29c53e78cb8310e9
TruongCuong <TruongCuong@CUONGTRUONG>
2014-11-22 23:34:25 +0700

    Initial Commit

--- DemoDuan1/src/com/cc/demoduan1/OnlineActivity.java
+++ DemoDuan1/src/com/cc/demoduan1/OnlineActivity.java
@@ -1,47 +1,18 @@
 package com.cc.demoduan1;
 
-import android.R;
 import android.app.Activity;
-import android.app.TabActivity;
-import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TabHost;
+import android.webkit.WebView;
 
-public class OnlineActivity extends TabActivity {
-
+public class OnlineActivity extends Activity {
+	public static String URL="http://www.nhaccuatui.com/";
+	WebView webview ;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Resources res = getResources(); // Resource object to get Drawables
-		TabHost tabHost = getTabHost(); // The activity TabHost
-		TabHost.TabSpec spec; // Resusable TabSpec for each tab
-		
-		Intent intent; // Reusable Intent for each tab
-		//
-		//Tạo các tab trong chương trình
-		intent = new Intent().setClass(this, TrangChuActivity.class);
-		spec = tabHost.newTabSpec("tab1").setIndicator("Trang Chủ",
-				res.getDrawable(R.drawable.ic_delete))
-				.setContent(intent);
-		tabHost.addTab(spec);
-		//
-		intent = new Intent().setClass(this, PlayListActivity.class);
-		spec = tabHost.newTabSpec("tab2").setIndicator("PLAYLIST",
-				res.getDrawable(R.drawable.ic_delete)).setContent(intent);
-		tabHost.addTab(spec);
-		tabHost.setCurrentTab(1);
-		//
-		intent = new Intent().setClass(this, BaiHatOLActivity.class);
-		spec = tabHost.newTabSpec("tab3").setIndicator("Bài Hát",
-				res.getDrawable(R.drawable.ic_delete)).setContent(intent);
-		tabHost.addTab(spec);
-		//
-		intent = new Intent().setClass(this, NgheSiOLActivity.class);
-		spec = tabHost.newTabSpec("tab4").setIndicator("Nghệ Sĩ",
-				res.getDrawable(R.drawable.ic_delete)).setContent(intent);
-		tabHost.addTab(spec);
-    }
+		setContentView(R.layout.activity_online);
+		webview = (WebView) findViewById(R.id.webview);
+		webview.getSettings().setJavaScriptEnabled(true);
+		webview.loadUrl(URL);
+}
 }
